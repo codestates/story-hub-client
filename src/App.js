0d9761d9a08b2e35ff3f0e4983f155da95b9d2fc
code-kay <kay8563@gmail.com>
2021-02-26 13:54:54 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import CoverPage from './pages/CoverPage';
+import BoardPage from './pages/BoardPage';
+import SearchPage from './pages/SearchPage';
+import AlertPage from './pages/AlertPage';
+import MyPage from './pages/MyPage';
+import EventPage from './pages/EventPage';
+import NewCommitPage from './pages/NewCommitPage';
+import CommitDetailPage from './pages/CommitDetailPage';
+import LoadingPage from './pages/LoadingPage';
+import NewStoryContentPage from './pages/NewStoryPage/content';
+import NewStoryInfoPage from './pages/NewStoryPage/info';
+import ContentPage from './pages/StoryDetailPage/content';
+import InfoPage from './pages/StoryDetailPage/info';
+import CommitPage from './pages/StoryDetailPage/commit';
+import CommentPage from './pages/StoryDetailPage/comment';
+
+
+
 function App() {
   return (
     <div className="App">
