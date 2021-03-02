c9acbcf0e33a3f6e6c58f455f119fdb3d2a72d32
code-kay <kay8563@gmail.com>
2021-03-02 11:23:16 +0900

    set

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    
+    * {
+        border: 1px solid rgb(224, 224, 224);
+    }
+
+    a {
+        text-decoration: none;
+        margin:1px;
+    }
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    
+    code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+        monospace;
+    }
+`
+
+export default GlobalStyle
