9ecd4b975bc96863c9ed536ebdd4a54207e71bf5
임관우 <gwanwoo@imgwan-uui-Macmini.local>
2021-03-04 09:24:07 +0900

    Styled-Component set

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-import buttonReducer from './buttonReducer';
 import pageReducer from './pageReducer';
 
 const rootReducer = combineReducers({
     userReducer,
-    buttonReducer,
     pageReducer,
 });
 export default rootReducer;
