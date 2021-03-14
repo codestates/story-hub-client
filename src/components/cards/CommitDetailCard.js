b5b35fe852bbb70ec73c040969a6e8e86d90285e
SungSeokMin <jkl154527@gmail.com>
2021-03-14 13:41:43 +0900

    📝 modify/commit_merge

--- src/components/cards/CommitDetailCard.js
+++ src/components/cards/CommitDetailCard.js
@@ -2,44 +2,43 @@ import React from 'react';
 import { useHistory, withRouter } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { commitDetailSaved,
-    commitDetailIndexSaved,
-    commitDetailTitleSaved,
-    commitDetailNicknameSaved,
-    commitDetailCreatedSaved,
-    commitDetailIsMergedSaved
+import {
+  commitDetailSaved,
+  commitDetailIndexSaved,
+  commitDetailTitleSaved,
+  commitDetailNicknameSaved,
+  commitDetailCreatedSaved,
+  commitDetailIsMergedSaved,
 } from '../../actions';
 
 const Card = styled.div`
-font-size: 2vw;
+  font-size: 2vw;
 
-.isMerged {
+  .isMerged {
     background-color: blue;
-}
+  }
 `;
 
 const CommitDetailCard = (props) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const handleCard = () => {
-        dispatch(commitDetailSaved(props.content))
-        dispatch(commitDetailIndexSaved(props.commit_index))
-        dispatch(commitDetailTitleSaved(props.title))
-        dispatch(commitDetailNicknameSaved(props.nickname))
-        dispatch(commitDetailCreatedSaved(props.created))
-        dispatch(commitDetailIsMergedSaved(props.merge_check))        
-        history.push('/commitdetail');
-    }
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleCard = () => {
+    dispatch(commitDetailSaved(props.content));
+    dispatch(commitDetailIndexSaved(props.commit_index));
+    dispatch(commitDetailTitleSaved(props.title));
+    dispatch(commitDetailNicknameSaved(props.nickname));
+    dispatch(commitDetailCreatedSaved(props.created));
+    dispatch(commitDetailIsMergedSaved(props.merge_check));
+    history.push('/commitdetail');
+  };
 
+  return (
+    <Card onClick={handleCard}>
+      <div className={props.merge_check === 1 ? 'isMerged' : ''}>
+        <button>{props.title}</button>
+      </div>
+    </Card>
+  );
+};
 
-    console.log(props.merge_check)
-    return (
-        <Card onClick = {handleCard}>
-            <div className = {props.merge_check==="1" ? "isMerged" : ""}>
-                <button>{props.title}</button>
-            </div>
-        </Card>
-        );
-    };
-    
-    export default withRouter(CommitDetailCard);
+export default withRouter(CommitDetailCard);
