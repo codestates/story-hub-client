31a10a8ab8dcc943ef58cca48e9399ee03c129f0
code-kay <73449966+code-kay@users.noreply.github.com>
2021-03-16 02:23:38 +0900

    Update comment.js

--- src/components/modals/MyPageModals/comment.js
+++ src/components/modals/MyPageModals/comment.js
@@ -28,7 +28,7 @@ const ModalFrame = styled.div`
     .close {
         align-self: start;
         margin: 25px 0 -35px 10px;
-        background-color: tr;
+        background-color: transparent;
         font-weight: 400;
         font-size: 1.5rem;
         border: none;
